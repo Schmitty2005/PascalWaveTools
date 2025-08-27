67bb704bbfb0c04ffc98d7755bdf85b272a4f315
Ubuntu2404LTS <brianschmitt001@yahoo.com>
2025-08-26 19:49:56 -0700

    Added Wavegen.pas with initial triange / sawtooth wave

--- WaveToolsTesting.lpr
+++ WaveToolsTesting.lpr
@@ -2,16 +2,29 @@
 program WaveToolsTesting;
 
 uses
-  SineGenInterfaces, SineGen, wavCalcs;
+  Classes,
+  SysUtils,
+  SineGenInterfaces,
+  SineGen,
+  wavCalcs,
+  waveGen;
 
 var
   wti: ICreateSineWave;
-  wtx : ICreateSquareWave;
+  wtx: ICreateSquareWave;
+  sPCM: TwavePCM;
+  ms: TBytesStream;
 begin
+  {HeapTrace is showing something is not getting free'd in interface!}
   wti := TsineGen<int8>.Create;
   wti.SineGenerator(22050, 660, 1000, 90);
+  {Also check for HeapTrace issues}
+  wtx := TsineGen<int16>.Create;
+  wtx.SquareGenerator(22050, 660, 1000, 90);
 
-  wtx := TsineGen<int16>.create;
-  wtx.SquareGenerator (22050, 660,1000,90);
-
+  {Test Triangle / Sawtooth WaveGen function}
+  writeln(triangleWave(sPCM, 800, 10, 44100, 1000, 0));
+  ms := TBytesStream.Create(Tbytes(sPCM));
+  ms.SaveToFile('triangle.pcm');
+  ms.Free;
 end.
---
+++ wavegen.pas
@@ -0,0 +1,59 @@
+unit waveGen;
+{$IFDEF FPC}
+{$mode Delphi}
+{$ENDIF}
+
+interface
+
+uses
+  {Math,} wavCalcs;
+
+type
+  TwavePCM = array of int16;
+
+  TwaveGenStyle = function(var aPCM: TwavePCM; const aHertz: integer;
+    const aAmp: int16; const aSampleRate: integer; const aMilliSecLength: uint32;
+    const aStartPhaseAngle: integer): uint32;
+
+function triangleWave(var aPCM: TwavePCM; const aHertz: integer;
+  const aAmp: int16; const aSampleRate: integer; const aMilliSecLength: uint32;
+  const aStartPhaseAngle: integer): uint32;
+
+
+implementation
+
+{$IFDEF FPC}
+uses Math;
+{$ENDIF}
+
+(*
+n = sample number
+p = period in samples
+n %p = triangle wave
+
+*)
+
+
+function triangleWave(var aPCM: TWavePCM; const aHertz: integer;
+  const aAmp: int16; const aSampleRate: integer; const aMilliSecLength: uint32;
+  const aStartPhaseAngle: integer): uint32;
+var
+  startRad: extended;
+  numSamples: uint32;
+  Count: uint32;
+  sample: int16;
+begin
+  startRad := DegToRad(aStartPhaseAngle);
+  numSamples := trunc(aMilliSecLength / 1000 * aSampleRate);
+  Count := 0;
+  setLength(aPcm, numSamples);
+  while Count < High(aPCM) do
+  begin
+    sample := Count mod aHertz;
+    sample := trunc(abs(sample) - aHertz / 2);
+    aPCM[Count] := 4 * aAmp * sample - aAmp;
+    Inc(Count);
+  end;
+end;
+
+end.
