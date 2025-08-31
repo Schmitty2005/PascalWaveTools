84f66aedb939232d940de58b053105f25601deab
Ubuntu2404LTS <brianschmitt001@yahoo.com>
2025-08-31 08:11:39 -0700

    Added use of TWaveStyleSpecs

--- wavegen.pas
+++ wavegen.pas
@@ -23,17 +23,24 @@ type
     StartPhaseDeg: integer;
   end;
 
-  TwaveGenStyle = function (aWaveSpec : TWaveStyleSpecs) : uInt32;
+  TwaveGenStyle = function(aWaveSpec: TWaveStyleSpecs): uint32;
 
   TwaveGenStyleExt = function(var aPCM: TwavePCM; const aHertz: integer;
     const aAmp: int16; const aSampleRate: integer; const aMilliSecLength: uint32;
     const aStartPhaseAngle: integer): uint32;
 
 
-
 function triangleWave(var aPCM: TwavePCM; const aHertz: integer;
   const aAmp: int16; const aSampleRate: integer; const aMilliSecLength: uint32;
-  const aStartPhaseAngle: integer = 90): uint32;
+  const aStartPhaseAngle: integer = 90): uint32; overload;
+
+function triangleWave(aWaveSpec: TWaveStyleSpecs): uint32; overload;
+
+function sawWave(var aPCM: TwavePCM; const aHertz: integer; const aAmp: int16;
+  const aSampleRate: integer; const aMilliSecLength: uint32;
+  const aStartPhaseAngle: integer = 90): uint32; overload;
+
+function sawWave(aWaveSpec: TWaveStyleSpecs): uint32; overload;
 
 implementation
 
@@ -54,7 +61,6 @@ Uses system.Math;
 { #todo -oB : Amplitude needs to change to 0 to 100 percent like other functions!  Currently uses Max of Int16 values.
  }
 
-
 function triangleWave(var aPCM: TwavePCM; const aHertz: integer;
   const aAmp: int16; const aSampleRate: integer; const aMilliSecLength: uint32;
   const aStartPhaseAngle: integer = 90): uint32;
@@ -72,125 +78,36 @@ begin
   while Count < High(aPCM) do
   begin
     Phase := fmod(Phase, tau);
-
     if Phase < PI then
       aPCM[Count] := round(aAmp * (2 / PI * (Phase - PI / 2)))
     else
       aPCM[Count] := round(aAmp * (-2 / PI * (Phase - 3 * PI / 2)));
-
     Phase := Phase + PhaseStep;
     Inc(Count);
   end;
   Result := Count;
 end;
 
-   (*
-
-   program TriangleWave;
-
-{$mode objfpc}
-
-uses
-  SysUtils;
-
-// Structure for the WAV file header
-type
-  TWaveHeader = packed record
-    riffID: array[0..3] of Char;
-    fileSize: dword;
-    waveID: array[0..3] of Char;
-    fmtID: array[0..3] of Char;
-    fmtSize: dword;
-    audioFormat: word;
-    numChannels: word;
-    sampleRate: dword;
-    byteRate: dword;
-    blockAlign: word;
-    bitsPerSample: word;
-    dataID: array[0..3] of Char;
-    dataSize: dword;
-  end;
-
-const
-  // Audio parameters
-  SampleRate = 44100;
-  BitDepth = 16;
-  NumChannels = 1; // Mono
-  Frequency = 440; // Hz
-  Duration = 5; // seconds
-
-var
-  WaveFile: file;
-  Header: TWaveHeader;
-  Samples: array of smallint;
-  NumSamples, i: longint;
-  Amplitude: longint;
-  Phase, PhaseStep: double;
-  Filename: string;
-
+function triangleWave(aWaveSpec: TWaveStyleSpecs): uint32;
 begin
-  Filename := 'triangle_wave.wav';
-  NumSamples := SampleRate * Duration;
-  SetLength(Samples, NumSamples);
+  Result := trianglewave(aWaveSpec.aPCM, aWaveSpec.FreqHertz,
+    aWaveSpec.Amplitude, aWaveSpec.SampleRate, aWaveSpec.LengthMilliSec,
+    aWaveSpec.StartPhaseDeg);
+end;
 
-  // Maximum amplitude for 16-bit signed integer
-  Amplitude := high(smallint);
+function sawWave(var aPCM: TwavePCM; const aHertz: integer; const aAmp: int16;
+  const aSampleRate: integer; const aMilliSecLength: uint32;
+  const aStartPhaseAngle: integer): uint32;
+begin
+  { #todo -oB : Needs Completion! }
+  result := 0;
+end;
 
-  // Phase step per sample for 440 Hz
-  PhaseStep := 2 * PI * Frequency / SampleRate;
-  Phase := 0;
-
-  // Generate the triangle wave samples
-  for i := 0 to NumSamples - 1 do
-  begin
-    // Create a sawtooth wave
-    // Use `fmod` to keep phase in [0, 2*PI]
-    Phase := fmod(Phase, 2 * PI);
-
-    // Convert sawtooth to triangle wave
-    // First, map phase to [-1, 1] range
-    if Phase < PI then
-      Samples[i] := round(Amplitude * (2 / PI * (Phase - PI / 2)))
-    else
-      Samples[i] := round(Amplitude * (-2 / PI * (Phase - 3 * PI / 2)));
-
-    // Advance phase
-    Phase := Phase + PhaseStep;
-  end;
-
-  // Create and write the WAV file
-  AssignFile(WaveFile, Filename);
-  Rewrite(WaveFile, 1); // Open in binary mode
-
-  // Populate the WAV header
-  Move('RIFF', Header.riffID, 4);
-  Header.fileSize := SizeOf(TWaveHeader) - 8 + NumSamples * SizeOf(smallint);
-  Move('WAVE', Header.waveID, 4);
-  Move('fmt ', Header.fmtID, 4);
-  Header.fmtSize := 16;
-  Header.audioFormat := 1; // PCM
-  Header.numChannels := NumChannels;
-  Header.sampleRate := SampleRate;
-  Header.byteRate := SampleRate * NumChannels * (BitDepth div 8);
-  Header.blockAlign := NumChannels * (BitDepth div 8);
-  Header.bitsPerSample := BitDepth;
-  Move('data', Header.dataID, 4);
-  Header.dataSize := NumSamples * SizeOf(smallint);
-
-  // Write header to file
-  BlockWrite(WaveFile, Header, SizeOf(Header));
-
-  // Write sample data to file
-  BlockWrite(WaveFile, Samples[0], NumSamples * SizeOf(smallint));
-
-  // Close file
-  CloseFile(WaveFile);
-
-  writeln('Triangle wave file "', Filename, '" created successfully.');
-end.
-   *)
-
-
-
+function sawWave(aWaveSpec: TWaveStyleSpecs): uint32;
+begin
+  Result := sawWave(aWaveSpec.aPCM, aWaveSpec.FreqHertz,
+    aWaveSpec.Amplitude, aWaveSpec.SampleRate, aWaveSpec.LengthMilliSec,
+    aWaveSpec.StartPhaseDeg);
+end;
 
 end.
