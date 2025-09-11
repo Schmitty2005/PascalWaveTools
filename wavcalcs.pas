2d0b18d0003762c744bbfddd59614f2d7f59a92b
Ubuntu2404LTS <brianschmitt001@yahoo.com>
2025-09-11 08:10:00 -0700

    added wavcalcs.pas

---
+++ wavcalcs.pas
@@ -0,0 +1,35 @@
+{$IFDEF FPC}
+{$mode Delphi}
+{$ENDIF}
+
+unit wavCalcs;
+
+
+interface
+{$IFDEF DCC}
+uses system.math;
+{$ENDIF}
+
+///<summary>  A quick inline function to calculate the number of samples that
+///would create one cycle at a specifice frequency in hertz;
+///</summary>
+///<param name="Hertz">The item to remove
+///</param>
+///<param name = "SampleRate"> The sample rate of the PCM / Wave data
+///</param>
+function calOneCycle(const Hertz: integer; const SampleRate: integer): integer; inline;
+
+implementation
+
+{$IFDEF FPC}
+uses Math;
+{$ENDIF}
+
+function calOneCycle(const hertz: integer; const sampleRate: integer): integer; inline;
+begin
+  Result := trunc(sampleRate / hertz);
+end;
+
+begin
+
+end.
