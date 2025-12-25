6d6226f521981ce99e0dcd0e994cf863d82644fe
Ubuntu2404LTS <brianschmitt001@yahoo.com>
2025-12-25 12:21:46 -0800

    Fixed dspTypes.pas and dspbitcrush.pas

--- dsp/dspTypes.pas
+++ dsp/dspTypes.pas
@@ -5,34 +5,25 @@ interface
 
 type
 
-  PbitCrushTo = ^TbitCrushTo;
-  TbitCrushTo = 2..14;
-
   PdspProcedure = ^TdspProcedure;
 
   TdspProcedure = procedure(var aPcm: array of int16;
-    beginIndex, endIndex: uInt64;
-    aDspData: Pointer);
+    const beginIndex, endIndex: uint64; const aDspData: Pointer);
 
   IdspProcess = interface
-    procedure process(aPcm: array of int16; aData: Pointer = nil);
+    procedure process(aPcm: array of int16; const aData: Pointer = nil);
   end;
 
   TdspBase = class abstract (TinterfacedObject, IdspProcess)
   private
-    fSampleRate: uInt32;
+    fSampleRate: uint32;
     fData: Pointer;
   public
     constructor Create(aSampleRate: uint32; aData: pointer = nil); virtual;
-    procedure process(aPcm: array of int16; aData: Pointer = nil); virtual; abstract;
+    procedure process(aPcm: array of int16; const aData: Pointer = nil);
+      virtual; abstract;
   end;
 
-  PbitCrushParam = ^TbitCrushParam;
-
-  TbitCrushParam = record
-    sourceDepth: byte;
-    crushDepth: byte;
-  end;
 
 implementation
 
---
+++ dsp/dspbitcrush.pas
@@ -0,0 +1,42 @@
+{$mode delphi}
+
+unit dspbitcrush;
+
+interface
+
+type
+
+  PbitCrushTo = ^TbitCrushTo;
+  TbitCrushTo = 2..14;
+
+  PbitCrushParam = ^TbitCrushParam;
+
+  TbitCrushParam = record
+    sourceDepth: byte;
+    crushDepth: byte;
+  end;
+
+procedure bitCrush(var aPcm: array of int16; const beginIndex, endIndex: uint64;
+  const aDspData: Pointer);
+
+implementation
+
+uses dspTypes;
+
+procedure bitCrush(var aPcm: array of int16; const beginIndex, endIndex: uint64;
+  const aDspData: Pointer);
+var
+  p: PbitcrushParam absolute aDspData;
+  x: uint64;
+begin
+  p^.CrushDepth := p^.sourceDepth - p^.CrushDepth;
+  writeln(p^.CrushDepth);
+  for x := beginIndex to endIndex do
+  begin
+    aPCM[x] := apcm[x] shr p^.CrushDepth;
+    aPCM[x] := apcm[x] shl p^.CrushDepth;
+    writeln(aPcm[x]);
+  end;
+end;
+
+end.
