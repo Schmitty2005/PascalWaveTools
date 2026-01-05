64d49127b236e90deec57fadee5bf81b5a8b34cd
Ubuntu2404LTS <brianschmitt001@yahoo.com>
2026-01-04 20:32:23 -0800

    Added dsp multithread skeleton

---
+++ dsp/dspmultithread.pas
@@ -0,0 +1,20 @@
+unit dspMultiThread;
+
+{$mode ObjFPC}{$H+}
+
+interface
+{Needs cthreads in main unit}
+uses
+  Classes, SysUtils;
+
+type
+  TdspThread = class (TThread)
+
+  end;
+
+implementation
+
+
+
+end.
+
